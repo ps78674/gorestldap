59584296033a6dbe887e5f8881810b5b1ef00d27
Pavel Sukhanov <mail.pavelsukhanov@gmail.com>
2022-09-01 15:39:10 +0300

    update src/ldap_helpers.go

--- src/ldap_helpers.go
+++ src/ldap_helpers.go
@@ -33,6 +33,14 @@ var (
 	}
 )
 
+// normalizeEntry returns entry in lowercase without spaces after comma
+// e.g. DC=test, dc=EXAMPLE,  dc=com -> dc=test,dc=example,dc=com
+func normalizeEntry(s string) string {
+	s = strings.ToLower(s)
+	re := regexp.MustCompile(`(,[\s]+)`)
+	return re.ReplaceAllString(s, ",")
+}
+
 // isCorrectDn checks dn syntax
 func isCorrectDn(s string) bool {
 	var allowedAttrs = []string{"cn", "uid", "ou", "dc"}
@@ -57,6 +65,8 @@ func isCorrectDn(s string) bool {
 	return true
 }
 
+// getEntryAttrNameSuffix returns entry attribute, name and suffix
+// e.g. for entry cn=admin,ou=users,dc=example,dc=com it would return ['cn', 'admin', 'ou=users,dc=example,dc=com']
 func getEntryAttrNameSuffix(entry string) (attr, name, suffix string) {
 	split := strings.SplitN(entry, ",", 2)
 	attrValuePair := strings.SplitN(split[0], "=", 2)
@@ -188,6 +198,98 @@ func applySearchFilter(o interface{}, f ldap.Filter) (bool, error) {
 	return false, nil
 }
 
+// get all attributes
+func getAllAttrsAndValues(o interface{}, operationalOnly bool) (ret []attrValues) {
+	rValue := reflect.ValueOf(o)
+	for i := 0; i < rValue.NumField(); i++ {
+		if _, ok := rValue.Type().Field(i).Tag.Lookup("ldap_skip"); ok {
+			continue
+		}
+		if _, ok := rValue.Type().Field(i).Tag.Lookup("ldap_operational"); (!ok && operationalOnly) || (ok && !operationalOnly) {
+			continue
+		}
+
+		tagValue := rValue.Type().Field(i).Tag.Get("json")
+		attr, _, _ := strings.Cut(tagValue, ",")
+		if attr == "" {
+			attr = rValue.Type().Field(i).Name
+		}
+
+		values := rValue.Field(i).Interface()
+
+		ret = append(ret, attrValues{Attr: attr, Values: values})
+	}
+
+	return ret
+}
+
+// get struct field values by field name
+func getAttrValues(o interface{}, fieldName string) (values interface{}) {
+	field, _ := reflect.TypeOf(o).FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
+	if _, ok := field.Tag.Lookup("ldap_skip"); ok {
+		return
+	}
+
+	rValue := reflect.ValueOf(o).FieldByName(field.Name)
+	if rValue.IsValid() {
+		values = rValue.Interface()
+	}
+
+	return
+}
+
+// create slice of ldap attributes
+func newLDAPAttributeValues(in interface{}) (out []ldap.AttributeValue) {
+	switch in := in.(type) {
+	case uint:
+		out = append(out, ldap.AttributeValue(fmt.Sprint(in)))
+	case string:
+		out = append(out, ldap.AttributeValue(in))
+	case []string:
+		for _, v := range in {
+			out = append(out, ldap.AttributeValue(v))
+		}
+	}
+	return
+}
+
+func createSearchResultEntry(o interface{}, attrs ldap.AttributeSelection, entryName string) (e ldap.SearchResultEntry) {
+	// set entry name
+	e.SetObjectName(entryName)
+
+	// // if no specific attributes requested -> add all attributes
+	if len(attrs) == 0 {
+		for _, v := range getAllAttrsAndValues(o, false) {
+			e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
+		}
+	}
+
+	// if some attributes requested -> add only those attributes
+	if len(attrs) > 0 {
+		for _, a := range attrs {
+			switch attr := strings.ToLower(string(a)); attr {
+			case "entrydn":
+				e.AddAttribute("entryDN", ldap.AttributeValue(entryName))
+			case "+":
+				for _, v := range getAllAttrsAndValues(o, true) {
+					e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
+				}
+			case "*":
+				for _, v := range getAllAttrsAndValues(o, false) {
+					e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
+				}
+			default:
+				values := getAttrValues(o, attr)
+				if values != nil {
+					e.AddAttribute(ldap.AttributeDescription(a), newLDAPAttributeValues(values)...)
+				}
+			}
+		}
+	}
+
+	return
+}
+
 // doCompare checks if object 'o' have attr 'attrName' with value 'attrValue'
 func doCompare(o interface{}, attrName string, attrValue string) (bool, error) {
 	fieldValue := reflect.ValueOf(o).FieldByNameFunc(func(s string) bool { return strings.EqualFold(s, attrName) })
@@ -226,29 +328,6 @@ func doCompare(o interface{}, attrName string, attrValue string) (bool, error) {
 	}
 
 	return false, nil
-}
-
-// create slice of ldap attributes
-func newLDAPAttributeValues(in interface{}) (out []ldap.AttributeValue) {
-	switch in := in.(type) {
-	case uint:
-		out = append(out, ldap.AttributeValue(fmt.Sprint(in)))
-	case string:
-		out = append(out, ldap.AttributeValue(in))
-	case []string:
-		for _, v := range in {
-			out = append(out, ldap.AttributeValue(v))
-		}
-	}
-	return
-}
-
-// normalizeEntry returns entry in lowercase without spaces after comma
-// e.g. DC=test, dc=EXAMPLE,  dc=com -> dc=test,dc=example,dc=com
-func normalizeEntry(s string) string {
-	s = strings.ToLower(s)
-	re := regexp.MustCompile(`(,[\s]+)`)
-	return re.ReplaceAllString(s, ",")
 }
 
 // doModify update object's 'o' attr 'attrName' with value 'attrValue'
@@ -296,81 +375,4 @@ func doModify(o interface{}, attrName string, values []ldap.AttributeValue) erro
 	root.Set(objCopy)
 
 	return nil
-}
-
-// get all attributes
-func getAllAttrsAndValues(o interface{}, operationalOnly bool) (ret []attrValues) {
-	rValue := reflect.ValueOf(o)
-	for i := 0; i < rValue.NumField(); i++ {
-		if _, ok := rValue.Type().Field(i).Tag.Lookup("ldap_skip"); ok {
-			continue
-		}
-		if _, ok := rValue.Type().Field(i).Tag.Lookup("ldap_operational"); (!ok && operationalOnly) || (ok && !operationalOnly) {
-			continue
-		}
-
-		tagValue := rValue.Type().Field(i).Tag.Get("json")
-		attr, _, _ := strings.Cut(tagValue, ",")
-		if attr == "" {
-			attr = rValue.Type().Field(i).Name
-		}
-
-		values := rValue.Field(i).Interface()
-
-		ret = append(ret, attrValues{Attr: attr, Values: values})
-	}
-
-	return ret
-}
-
-// get struct field values by field name
-func getAttrValues(o interface{}, fieldName string) (values interface{}) {
-	field, _ := reflect.TypeOf(o).FieldByNameFunc(func(n string) bool { return strings.EqualFold(n, fieldName) })
-	if _, ok := field.Tag.Lookup("ldap_skip"); ok {
-		return
-	}
-
-	rValue := reflect.ValueOf(o).FieldByName(field.Name)
-	if rValue.IsValid() {
-		values = rValue.Interface()
-	}
-
-	return
-}
-
-func createSearchResultEntry(o interface{}, attrs ldap.AttributeSelection, entryName string) (e ldap.SearchResultEntry) {
-	// set entry name
-	e.SetObjectName(entryName)
-
-	// // if no specific attributes requested -> add all attributes
-	if len(attrs) == 0 {
-		for _, v := range getAllAttrsAndValues(o, false) {
-			e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
-		}
-	}
-
-	// if some attributes requested -> add only those attributes
-	if len(attrs) > 0 {
-		for _, a := range attrs {
-			switch attr := strings.ToLower(string(a)); attr {
-			case "entrydn":
-				e.AddAttribute("entryDN", ldap.AttributeValue(entryName))
-			case "+":
-				for _, v := range getAllAttrsAndValues(o, true) {
-					e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
-				}
-			case "*":
-				for _, v := range getAllAttrsAndValues(o, false) {
-					e.AddAttribute(ldap.AttributeDescription(v.Attr), newLDAPAttributeValues(v.Values)...)
-				}
-			default:
-				values := getAttrValues(o, attr)
-				if values != nil {
-					e.AddAttribute(ldap.AttributeDescription(a), newLDAPAttributeValues(values)...)
-				}
-			}
-		}
-	}
-
-	return
 }
